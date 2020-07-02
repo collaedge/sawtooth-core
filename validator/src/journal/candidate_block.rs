719ea2c36b2a0116cfc038fb610fd4c13488a4c9
collaedge <a125614418@outlook.com>
2020-07-02 18:34:31 +1000

    Update candidate_block.rs

--- validator/src/journal/candidate_block.rs
+++ validator/src/journal/candidate_block.rs
@@ -168,7 +168,7 @@ impl CandidateBlock {
     fn check_transaction_rewards(&self,  txn: &Transaction) -> bool {
         // get all record from chian
         let temp = self.commit_store.get_block_count();
-        total_blocks = temp.unwrap();
+        let total_blocks = temp.unwrap();
         let mut blocks: Vec<Block> = Vec::new(); 
         let x = 1;
         while x<total_blocks {
