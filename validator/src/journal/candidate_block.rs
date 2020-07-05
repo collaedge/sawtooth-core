5b4595cc37cdbc63984ed501114b88132b1e0016
collaedge <a125614418@outlook.com>
2020-07-05 02:23:26 +0000

    update candidate_block

--- validator/src/journal/candidate_block.rs
+++ validator/src/journal/candidate_block.rs
@@ -189,7 +189,7 @@ impl CandidateBlock {
         let mut txn_data: Vec<String> = Vec::new();
         for t in transactions {
             // println!("======= history transation ========= {:#?}", str::from_utf8(&t).unwrap());
-            let txn_str = str::from_utf8(&t).unwrap();
+            let txn_str = String::from_utf8(t).unwrap();
             let data:Vec<&str> = txn_str.split(',').collect();
             for v in data {
                 txn_data.push(v.to_string());
